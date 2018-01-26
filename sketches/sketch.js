43479ca3546cdd41aea86dedd35a11c03da9bbd8
michellelsfong <michellelsfong@gmail.com>
2018-01-25 21:05:03 -0800

    Play sound

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,27 +1,17 @@
-var value = 0;
+var song;
 
 function setup() {
-<<<<<<< HEAD
-  aKey = loadSound('assets/keys/a.mp3');
+  song = loadSound('assets/key/a.mp3');
   createCanvas(720, 200);
   background(255,0,0);
 }
 
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    aKey.play();
-  } else if (keyCode === RIGHT_ARROW) {
-    value = 0;
-=======
-  createCanvas(640, 480);
-}
-
-function draw() {
-  if (keyIsPressed === true) {
-    fill(0);
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
   } else {
-    fill(255);
->>>>>>> parent of 3eab3b3... sound
+    song.play();
+    background(0,255,0);
   }
-  rect(25, 25, 50, 50);
 }
