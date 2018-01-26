cd83aa96efa440401023f87b80a99cca623c1069
michellelsfong <michellelsfong@gmail.com>
2018-01-25 21:59:42 -0800

    h

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,10 +1,12 @@
 var aSound;
 var bSound;
+var hSound;
 var value = 0;
 
 function setup() {
   aSound = loadSound('assets/keys/a.mp3');
   bSound = loadSound('assets/keys/b.mp3');
+  hSound = loadSound('assets/keys/h.mp3');
   createCanvas(720, 200);
   background(255,0,0);
 }
@@ -19,12 +21,14 @@ function keyTyped() {
     value = 255;
     aSound.setVolume(0.1);
     aSound.play();
-  }
-
-  else if (key === 'b') {
+  } else if (key === 'b') {
     value = 255;
     bSound.setVolume(0.1);
     bSound.play();
+  } else if (key === 'h') {
+    value = 255;
+    hSound.setVolume(0.1);
+    hSound.play();
   }
 
   return false;
