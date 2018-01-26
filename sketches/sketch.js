e0b2f468dfe409daee0472d2f4874ede3c6e3d39
michellelsfong <michellelsfong@gmail.com>
2018-01-25 21:02:47 -0800

    Revert "sound"
    This reverts commit 3eab3b3f4484fdca547e06d40501fa296c73bb6b.
    
    # Conflicts:
    #	sketches/sketch.js

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,6 +1,7 @@
 var value = 0;
 
 function setup() {
+<<<<<<< HEAD
   aKey = loadSound('assets/keys/a.mp3');
   createCanvas(720, 200);
   background(255,0,0);
@@ -11,5 +12,16 @@ function keyPressed() {
     aKey.play();
   } else if (keyCode === RIGHT_ARROW) {
     value = 0;
+=======
+  createCanvas(640, 480);
+}
+
+function draw() {
+  if (keyIsPressed === true) {
+    fill(0);
+  } else {
+    fill(255);
+>>>>>>> parent of 3eab3b3... sound
   }
+  rect(25, 25, 50, 50);
 }
