628a23026d9ac7d718fcc24051170d2ab326f845
michellelsfong <michellelsfong@gmail.com>
2018-01-25 20:59:01 -0800

    a key

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,15 +1,25 @@
+var value = 0;
+
 function setup() {
-  song = loadSound('assets/keys/a.mp3');
+  aKey = loadSound('assets/keys/a.mp3');
   createCanvas(720, 200);
   background(255,0,0);
 }
 
-function mousePressed() {
-  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-    song.stop();
-    background(255,0,0);
-  } else {
-    song.play();
-    background(0,255,0);
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+
+function keyPressed() {
+  if (keyCode === a) {
+    aKey.play();
+  } else if (keyCode === RIGHT_ARROW) {
+    value = 0;
   }
+}
+
+function keyPressed() {
+  // Do something
+  return false; // prevent any default behaviour
 }
