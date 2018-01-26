2d4ff54b44e5bbe7fa29fba3894d045af6b6f38e
michellelsfong <michellelsfong@gmail.com>
2018-01-25 22:02:01 -0800

    b

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -21,6 +21,10 @@ function keyTyped() {
     value = 255;
     aSound.setVolume(0.1);
     aSound.play();
+  } else if (key === 'b') {
+    value = 255;
+    bSound.setVolume(0.1);
+    bSound.play();
   } else if (key === 'h') {
     value = 255;
     hSound.setVolume(0.1);
